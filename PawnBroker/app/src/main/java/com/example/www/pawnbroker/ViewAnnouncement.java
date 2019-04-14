1006ad096a2eb0be27a40967fa8adad83d6199a9
daniyal1217 <38786176+daniyal1217@users.noreply.github.com>
2019-04-14 06:22:27 +0500

    nothing new. just 1 small bug removed

--- PawnBroker/app/src/main/java/com/example/www/pawnbroker/ViewAnnouncement.java
+++ PawnBroker/app/src/main/java/com/example/www/pawnbroker/ViewAnnouncement.java
@@ -14,7 +14,7 @@ public class ViewAnnouncement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_forall);
-        lv=(ListView)findViewById(R.id.lv);
+        lv=(ListView)findViewById(R.id.lvUsers);
         ArrayAdapter ap = new ArrayAdapter(ViewAnnouncement.this,android.R.layout.simple_list_item_1,new AnnouncementsCRUD(ViewAnnouncement.this).viewAllAnnouncements());
         lv.setAdapter(ap);
     }
